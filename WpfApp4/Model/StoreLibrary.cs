e81f065bc3df5f87ec94331e7de4f3c3958caab9
tim <1chloeprice@gmail.com>
2021-12-02 21:06:55 +0200

    1

---
+++ WpfApp4/Model/StoreLibrary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace reader
+{
+    static class StoreLibrary
+    {
+        static List<Book> BooksToSell = new List<Book>();
+        
+        static public void AddBook(Book item)
+        {
+            BooksToSell.Add(item);
+        }
+        static public void AddAllBooks(string path)
+        {
+            string[] allBooks = Directory.GetFiles(path);
+            Book tempItem;
+
+           for(int a = 0;a<allBooks.Length;a++)
+            {
+                tempItem = new(allBooks[a]);
+            }
+
+        }
+    }
+}
